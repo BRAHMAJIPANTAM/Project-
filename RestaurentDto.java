7b5dc6c2c2417063634caacb2b31743100a79705
BRAHMAJIPANTAM <brahmajipantham90@gmail.com>
2024-09-16 15:34:03 +0530

    Add files via upload

---
+++ RestaurentDto.java
@@ -0,0 +1,19 @@
+package java;
+
+public class RestaurentDto {
+	int Restaurent_id;
+	String restaurent_name;
+	String address;
+	String location;
+	String open_time;
+	 String close_time;
+	 String gst_number;
+	 String cuisins_list;
+	int contact_number;
+	 int bank_account_number;
+	String email;
+	String login_password;
+	 float avg_rating;
+	String curr_order_accepting_status;
+
+}
